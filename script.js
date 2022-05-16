c2774bfb7c211f8f79abe4c3ecea03be0d54a616
ABISHEK A S <80886859+abishek7278@users.noreply.github.com>
2022-05-16 15:23:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+ 
+const result = document.querySelector('.cal-print')
+const clear =  document.querySelector('#clear')
+const compute =  document.querySelector('.btn-equals')
+
+
+
+const buttons = document.querySelectorAll('.num, .op')
+// console.log(buttons)
+let input = [];
+
+//Array.form() to convert buttons to Array  
+//loop throungh the button  to acces each
+Array.from(buttons).forEach((button)=>{
+
+//Add event  listener to button
+button.addEventListener('click', ()=>{
+ const operator = ['+', '-', '/', '*']
+ //display result and checking if 
+ if (input[input.length-1] == '=' && !operator.includes(button.value)){
+     result.value  = button.value;
+ }else{
+     result.value += button.value;
+ }
+input.push(button.value)
+})
+
+})
+
+// add event listener click to (=)
+compute.addEventListener('click', ()=>{
+ input.push('=');
+ //checking 
+ if(result.value.length == 0){
+     return false
+ }else{
+     result.value = eval(result.value)
+ }
+
+})
+
+// clear the screen 
+clear.addEventListener('click', () =>{
+    result.value=''
+})
